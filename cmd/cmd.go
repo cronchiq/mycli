486581e936c05ee8df709b2bb837202b3f208d6d
cronchiq <macaa.toth@gmail.com>
2022-01-31 12:04:22 +0100

    feat: add SUM command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	_ "github.com/cronchiq/mycli/cmd/hello"
 	_ "github.com/cronchiq/mycli/cmd/ahoj"
+	_ "github.com/cronchiq/mycli/cmd/sum"
 	"github.com/cronchiq/mycli/cmd/root"	
 	"github.com/spf13/cobra"
 )
