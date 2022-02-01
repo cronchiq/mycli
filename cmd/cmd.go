2b781ffc9cbe85ee1f8e23b32cddba363c2d9108
cronchiq <macaa.toth@gmail.com>
2022-02-01 10:18:30 +0100

    feat: Add Args to Say command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,18 +1,19 @@
 package cmd
 
 import (
-	_ "github.com/cronchiq/mycli/cmd/hello"
 	_ "github.com/cronchiq/mycli/cmd/calc"
-	_ "github.com/cronchiq/mycli/cmd/calc/sum"
 	_ "github.com/cronchiq/mycli/cmd/calc/diff"
-	_ "github.com/cronchiq/mycli/cmd/calc/multiply"
 	_ "github.com/cronchiq/mycli/cmd/calc/divide"
+	_ "github.com/cronchiq/mycli/cmd/calc/multiply"
+	_ "github.com/cronchiq/mycli/cmd/calc/sum"
+	_ "github.com/cronchiq/mycli/cmd/hello"
+
 	// _ "github.com/cronchiq/mycli/cmd/ahoj"
+	"github.com/cronchiq/mycli/cmd/root"
 	_ "github.com/cronchiq/mycli/cmd/say"
 	_ "github.com/cronchiq/mycli/cmd/say/ahoj"
 	_ "github.com/cronchiq/mycli/cmd/say/hello"
 	_ "github.com/cronchiq/mycli/cmd/version"
-	"github.com/cronchiq/mycli/cmd/root"	
 	"github.com/spf13/cobra"
 )
 
--- cmd/say/say.go
+++ cmd/say/say.go
@@ -1,15 +1,19 @@
 package say
 
 import (
+	"fmt"
+
 	"github.com/cronchiq/mycli/cmd/root"
 	"github.com/spf13/cobra"
 )
 
-var RootCmdFlagJson bool
-
 var Cmd = &cobra.Command{
 	Use:   "say",
 	Short: "Say something",
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Println(args[0])
+	},
 }
 
 func init() {
